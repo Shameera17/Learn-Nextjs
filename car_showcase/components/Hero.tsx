4a21b3ecede360839d3ff1d0485e4470f0df9f8e
Shameera17 <shameera.carrim17@gmail.com>
2023-10-08 16:29:33 +0530

    added changes

--- car_showcase/components/Hero.tsx
+++ car_showcase/components/Hero.tsx
@@ -1,6 +1,40 @@
-// import Image from 'next/image'
+"use client";
+
+import { Button } from ".";
+
+import Image from "next/image";
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        {/* bem methodology  */}
+        <h1 className="hero__title">
+          Find, book, or rent a car — quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process
+        </p>
+        <Button
+          title={"Explore cars"}
+          containerStyles={"bg-primary-blue text-white rounded-full mt-10"}
+          handleClick={handleScroll}
+        />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image
+              src="/hero.png"
+              alt={"hero"}
+              fill
+              className="object-contain"
+            />
+            <div className="hero__image-overlay" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
